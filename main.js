09ad66c3329e2efe6bcff3dc6255353332eee674
peax3 <peaceadjerese@gmail.com>
2021-04-17 10:30:45 +0100

    add: set public as static folder and server index.html

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+const path = require("path");
 const express = require("express");
+const xss = require("xss-clean");
 const helmet = require("helmet");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -21,9 +23,13 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(xss());
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // register routes
 app.use("/api/auth", authRoutes);
